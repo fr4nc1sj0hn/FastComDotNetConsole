5b75cd2678f6221d424d52c28e3f4f9fe7ff8542
FRANC583 <FRANC583@LPC-D78PYYF2>
2017-11-13 11:14:02 +0800

    Init

---
+++ FastComDotNetConsole/FastComDotNetConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FastComDotNetConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
